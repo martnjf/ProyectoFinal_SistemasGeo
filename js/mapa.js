ef5d17800fe6dfc23b0747e3b1032951d941e244
martnjf <martin.jassof28@gmail.com>
2021-06-07 22:46:58 -0500

    *o*

--- js/mapa.js
+++ js/mapa.js
@@ -1,5 +1,5 @@
 function iniciaMapa(){
-
+    var informacion = "<h1>Aquí lo esperamos</h1>"
     var styledMapType = new google.maps.StyledMapType(
         [
             {
@@ -281,7 +281,7 @@ function iniciaMapa(){
 
     var propiedades = {
         center: { 
-             lat: 21.152639, lng:  -101.711598
+             lat: 20.94542803736514, lng: -101.43458073056293
         },
         zoom: 14,
         mapTypeControlOptions: {
@@ -291,9 +291,39 @@ function iniciaMapa(){
     };
 
     var mapa = document.getElementById("map");
-
     var map = new google.maps.Map(mapa, propiedades);
 
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            let posicion = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+
+            let propiedadesMarcador = {
+                position: posicion,
+                map,
+                title: "Marcador"
+            }
+
+            const marcador = new google.maps.Marker(propiedadesMarcador);
+
+            map.setCenter(posicion);
+
+            const infowindow = new google.maps.InfoWindow({
+                content: informacion
+            })
+
+            marcador.addListener("click", ()=>{
+                infowindow.open(map, marcador);
+            }) 
+
+            
+        });
+    }
+    let posicion = {
+        lat : 20.94542803736514, lng : -101.43458073056293
+    }
     map.mapTypes.set('style_map', styledMapType);
     map.setMapTypeId('style_map');
 }
