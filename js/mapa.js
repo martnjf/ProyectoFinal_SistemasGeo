046ab48f14cd943682fa118077dd55a27485031b
martnjf <martin.jassof28@gmail.com>
2021-06-14 11:38:43 -0500

    sSIUUUU

--- js/mapa.js
+++ js/mapa.js
@@ -1,3 +1,7 @@
+var informacion =  "<h1>Autopartes Gadi</h1>"
+    informacion += "<p><strong>Dirección:</strong> Calzada Hidalgo 80, Col. Centro, 36100 Silao, Gto, México.</p>"
+    informacion += "<p><strong>Teléfono:</strong>472 117 1427.</p>" 
+
 function iniciaMapa(){
     var propiedades = {
         center: {
@@ -13,9 +17,20 @@ function iniciaMapa(){
         lat : 20.94562937009317, lng : -101.43458139735137
     }
 
-    const marker = new google.maps.Marker({
-        position: posicion, 
+    let propiedadesMarcador = {
+        position: posicion,
         map,
-        title: "Gadi"
-    });
+        title: "Marcador"
+    }
+
+    const marker = new google.maps.Marker(propiedadesMarcador);
+    map.setCenter(posicion)
+    
+    const infowindow = new google.maps.InfoWindow({
+        content : informacion
+    })
+
+    marcador.addListener("click", ()=>{
+        infowindow.open(map,marcador);
+    })
 }
